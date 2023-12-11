d783f76a9562408da26771d0a416b3b66deeade6
unknown <m.r.fouladi7@gmail.com>
2023-12-11 13:40:14 +0330

    block event loop and use it on timer route handler

--- server.js
+++ server.js
@@ -4,6 +4,22 @@ const app = express();
 
 const PORT = process.env.PORT || 3300;
 
+function delay(duration) {
+  const startTime = Date.now();
+  while (Date.now() - startTime < duration) {
+    //Event loop is blocked
+  }
+}
+
+app.get("/", (req, res) => {
+  res.send("Performance Example");
+});
+
+app.get("/timer", (req, res) => {
+  delay(9000);
+  res.send("event loop was block for 9 seconds");
+});
+
 app.listen(PORT, () => {
   console.log(`server is listening to ${PORT}`);
 });
